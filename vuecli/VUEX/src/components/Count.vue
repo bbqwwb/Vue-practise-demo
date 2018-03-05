7608a1e01275c57607726135379edfe14646e114
bbqwwb <946517956@qq.com>
2018-03-05 16:40:10 +0800

    no mes

--- vuecli/VUEX/src/components/Count.vue
+++ vuecli/VUEX/src/components/Count.vue
@@ -1,21 +1,32 @@
 <template>
     <div>
       <div>{{msg}}</div>
-      <div>{{$store.state.count}}</div>
+      <div>{{$store.state.count}}-{{count}}</div><!--后面这种更简洁的方法 需要用mapState-->
       <p>
-        <button @click="$store.commit('add')">+</button>
-        <button @click="$store.commit('reduce')">-</button>
+         <button @click="$store.commit('add',10)">+10</button> <!--可以理解为 add方法的第一个参数state是默认的不需要传  直接commit方法名 后面跟参数-->
+        <button @click="$store.commit('reduce')">-1</button>
+        <button @click="reduce">-1</button><!--这种更简洁的写法  需要用mapMutations-->
+        <button @click="add(2)">+2</button>
       </p>
     </div>
 </template>
 <script>
 import store from '@/vuex/store';
+import {mapState,mapMutations,mapGetters} from 'vuex'; //有坑 必须加{}
 export default {
   data(){
     return{
       msg:'hello vuex'
     }
   },
+  computed:{   //ES6的写法 使用ES6中的展开运算符"…"
+    ...mapState(['count']),
+    // count(){
+    //   return this.$store.getters.count
+    // }
+    ...mapGetters(['count'])
+  },
+  methods:mapMutations(['add','reduce']),
   store
 }
 </script>
--- vuecli/VUEX/src/vuex/store.js
+++ vuecli/VUEX/src/vuex/store.js
@@ -8,15 +8,21 @@ const state={
 }
 
 const mutations={
-  add(state){
-      state.count++;
+  add(state,n){
+      state.count+=n;
   },
   reduce(state){
       state.count--;
   }
 }
 
+const getters={
+  count:function(state){
+    return state.count+=100;
+  }
+  //count:state=>state.count+=100 //ES6写法 箭头函数
+}
+
 export default new Vuex.Store({//这里有坑 Store首字母要大写
-  state,
-  mutations //全部都要export才能使用
+  state,mutations,getters, //全部都要export才能使用
 })
