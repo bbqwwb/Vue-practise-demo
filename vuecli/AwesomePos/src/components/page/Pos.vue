ea55d508aa7252f963409eed7ccba0ced1a0cd03
bbqwwb <946517956@qq.com>
2018-03-07 16:26:20 +0800

    no mes

--- vuecli/AwesomePos/src/components/page/Pos.vue
+++ vuecli/AwesomePos/src/components/page/Pos.vue
@@ -8,8 +8,18 @@
               <el-table-column prop="goodsName" label="商品" align="center"></el-table-column>
               <el-table-column prop="count" label="数量" align="center" width="50"></el-table-column>
               <el-table-column prop="price" label="金额" align="center" width="70"></el-table-column>
-              <el-table-column  label="操作" align="center" width="120" fixed="right"></el-table-column>
+              <el-table-column  label="操作" align="center" width="120" fixed="right">
+                <template slot-scope="scope">
+                  <el-button type="text" size="small">删除</el-button>
+                  <el-button type="text" size="small">增加</el-button>
+                </template>
+              </el-table-column>
             </el-table>
+            <div class="div-btn">
+              <el-button type="warning">挂单</el-button>
+              <el-button type="danger">删除</el-button>
+              <el-button type="success">结账</el-button>
+            </div>
           </el-tab-pane>
           <el-tab-pane label="挂单" name="second">挂单</el-tab-pane>
           <el-tab-pane label="外卖" name="third">外卖</el-tab-pane>
@@ -47,17 +57,19 @@ export default {
       ]
     }
   },
-  mounted:function(){
-    var orderHeight = document.body.clientHeight;
-    document.getElementById("order-list").style.height = orderHeight+"px";
-  }
+  // mounted:function(){
+  //   var orderHeight = document.body.clientHeight;
+  //   document.getElementById("order-list").style.height = orderHeight+"px";
+  // }
 }
 </script>
 <style>
 .pos-layout{height: 100%;}
+.pos-layout .el-row{height: 100%;}
 .pos-order{ background: #f9fafc; border-right: 1px solid #c0ccda;height: 100%;}
 #tab-first{margin-left: 20px;}
 .el-tabs__active-bar{left:20px;}
+.div-btn{margin-top: 10px;}
 </style>
 
 
